0cc11efb68a08128be03ace465331ed559316416
Neriyan Nadasen <neriyan@gmail.com>
2021-05-15 17:04:56 +0200

    Changed hello message

--- src/main/java/bamboo/Bamboo.java
+++ src/main/java/bamboo/Bamboo.java
@@ -3,6 +3,6 @@ package bamboo;
 public class Bamboo {
 
     public static void main (String[] args){
-        System.out.println("Hello from applcation deployed by Bamboo");
+        System.out.println("Hello world from applcation deployed by Bamboo");
     }
 }
